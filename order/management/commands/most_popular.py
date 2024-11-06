1761030e78f224badad6db7b155a6b7b4a3ddb2a
hella753 <kristi.gaphrindashvili@gmail.com>
2024-11-06 22:41:21 +0400

    first version

--- order/management/commands/most_popular.py
+++ order/management/commands/most_popular.py
@@ -1,9 +1,25 @@
 from django.core.management.base import BaseCommand
 from order.models import CartItem
+from store.models import Product
 
 
 class Command(BaseCommand):
     help = "Finds top 3 products for users"
 
     def handle(self, *args, **options):
-        pass
+        list_of_amounts = []
+        dct = {}
+        for product in Product.objects.iterator():
+            amount = CartItem.objects.filter(product=product).count()
+            dct.update({product:amount})
+            list_of_amounts.append(amount)
+        sorted_list = sorted(list_of_amounts, reverse=True)[:3]
+        result = []
+        for num in sorted_list:
+            for key, value in dct.items():
+                if num==value:
+                    result.append((key, value))
+
+        self.stdout.write(
+            self.style.SUCCESS(f"{result}")
+        )
