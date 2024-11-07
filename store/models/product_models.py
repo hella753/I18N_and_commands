4051f9a1e4244f9071be0dca0ec10a54f0c0c474
hella753 <kristi.gaphrindashvili@gmail.com>
2024-11-07 15:26:14 +0400

    turned the models files into packages

--- store/models/product_models.py
+++ store/models/product_models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
-from django.conf import settings
 from versatileimagefield.fields import VersatileImageField
 from django.utils.translation import gettext_lazy as _
 
@@ -79,34 +78,6 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.product_name}"
-
-
-class ProductReviews(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        verbose_name=_("ავტორი")
-    )
-    product = models.ForeignKey(
-        "Product",
-        on_delete=models.CASCADE,
-        verbose_name=_("პროდუქტი")
-    )
-    date = models.DateField(auto_now_add=True, verbose_name=_("თარიღი"))
-    review = models.TextField(verbose_name=_("რევიუ"))
-
-
-class ShopReviews(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        verbose_name=_("ავტორი")
-    )
-    review = models.TextField(verbose_name=_("რევიუ"))
-    date = models.DateField(auto_now_add=True, verbose_name=_("თარიღი"))
-
-    def __str__(self):
-        return f"{self.id}"
 
 
 class ProductTags(models.Model):
